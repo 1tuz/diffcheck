64d1abee370c5abe9a82da779b024a54db660b5e
Antony <58532577+1tuz@users.noreply.github.com>
2023-01-25 10:04:50 +0300

    Update diff.sh

--- diff.sh
+++ diff.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
-# Take first link as input
-echo "Enter first link:"
-read link1
+if [ $# -eq 1 ]
+then
+    read -p "Enter the second URL: " url2
+    url1=${1#"curl "}
+    url2=${url2#"curl "}
+elif [ $# -eq 0 ]
+then
+    read -p "Enter the first URL: " url1
+    url1=${url1#"curl "}
+    read -p "Enter the second URL: " url2
+    url2=${url2#"curl "}
+else
+    url1=${1#"curl "}
+    url2=${2#"curl "}
+fi
 
-# Take second link as input
-echo "Enter second link:"
-read link2
+# Make the first request
+response1=$(curl -s "$url1")
 
-# Use curl to retrieve contents of both links
-curl -s $link1 > link1.json
-curl -s $link2 > link2.json
+# Make the second request
+response2=$(curl -s "$url2")
 
-# Compare the difference between the two files using diff
-diffchecker link1.json link2.json
+if [ -z "$response1" ] || [ -z "$response2" ]
+then
+    echo "Error: One of the URLs is not valid"
+    exit 1
+else
+    # Compare the responses and store the output in a variable
+    diff_output=$(npx diffchecker "$response1" "$response2")
 
-# Print message indicating the difference has been saved
-echo "Difference saved in diff.json"
+    # Output the diff_output to check it
+    echo $diff_output
 
+    #Upload the output to diffchecker
+    npx diffchecker -u $diff_output
+fi
